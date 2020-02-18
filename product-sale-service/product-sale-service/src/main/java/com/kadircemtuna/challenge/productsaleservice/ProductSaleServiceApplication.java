ee314afb894947141494a50b4a00070f95220d02
Kadir Cem Tuna <kadir.tuna@etiya.com>
2020-02-18 17:05:38 +0300

    Swagger added

--- product-sale-service/product-sale-service/src/main/java/com/kadircemtuna/challenge/productsaleservice/ProductSaleServiceApplication.java
+++ product-sale-service/product-sale-service/src/main/java/com/kadircemtuna/challenge/productsaleservice/ProductSaleServiceApplication.java
@@ -6,9 +6,11 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSwagger2
 public class ProductSaleServiceApplication {
 
   @Bean
