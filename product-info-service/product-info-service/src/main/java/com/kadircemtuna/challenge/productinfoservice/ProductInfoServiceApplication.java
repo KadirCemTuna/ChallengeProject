9513010587f355ee14df7bad8a58fb03c94bc3a7
Kadir Cem Tuna <kadir.tuna@etiya.com>
2020-02-18 16:35:21 +0300

    Eureka client conf

--- product-info-service/product-info-service/src/main/java/com/kadircemtuna/challenge/productinfoservice/ProductInfoServiceApplication.java
+++ product-info-service/product-info-service/src/main/java/com/kadircemtuna/challenge/productinfoservice/ProductInfoServiceApplication.java
@@ -2,10 +2,12 @@ package com.kadircemtuna.challenge.productinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductInfoServiceApplication {
 
 	@Bean
