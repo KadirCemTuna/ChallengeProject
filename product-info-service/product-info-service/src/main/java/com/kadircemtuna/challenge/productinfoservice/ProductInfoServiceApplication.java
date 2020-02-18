fa862b380f1d4ec8bfa5843557c4a622dc144515
Kadir Cem Tuna <kadir.tuna@etiya.com>
2020-02-18 17:32:20 +0300

    Swagger conf

--- product-info-service/product-info-service/src/main/java/com/kadircemtuna/challenge/productinfoservice/ProductInfoServiceApplication.java
+++ product-info-service/product-info-service/src/main/java/com/kadircemtuna/challenge/productinfoservice/ProductInfoServiceApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -16,4 +20,11 @@ public class ProductInfoServiceApplication {
 		SpringApplication.run(ProductInfoServiceApplication.class, args);
 	}
 
+	@Bean
+	public Docket swaggerConfiguration(){
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.kadircemtuna"))
+				.build();
+	}
 }
